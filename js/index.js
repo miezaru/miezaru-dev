1a83e6b6a318b24f37b4b36a7a2afa22482128e6
Artem Usatyi <miezaruexpr@gmail.com>
2024-07-08 17:19:29 +0300

    miezaru.dev | Add main-menu, page-menu and do some changes

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const headerEl = document.querySelector('.header');
 const sectionHeroEl = document.querySelector('.section-hero');
 
-const observer = new IntersectionObserver(
+const observerLogo = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     if (!ent.isIntersecting) {
@@ -19,4 +19,73 @@ const observer = new IntersectionObserver(
     rootMargin: '-80%',
   }
 );
-observer.observe(sectionHeroEl);
+observerLogo.observe(sectionHeroEl);
+
+const observerMenuBtn = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      headerEl.classList.add('menu-bottom');
+    } else {
+      headerEl.classList.remove('menu-bottom');
+    }
+  },
+  {
+    // inside in the viewport
+    root: null,
+    // as soon as 0% sectionHeroEl in viewport (moves out completely)
+    threshold: 0,
+    rootMargin: '-80%',
+  }
+);
+observerMenuBtn.observe(sectionHeroEl);
+
+// main-menu
+const mainMenuBtn = document.querySelector('.main-nav__btn');
+const mainMenuLinks = document.querySelectorAll('.main-nav__link');
+
+mainMenuBtn.addEventListener('click', () => {
+  if (headerEl.classList.contains('menu-open')) {
+    headerEl.classList.remove('menu-open');
+    return;
+  } else {
+    headerEl.classList.toggle('menu-open');
+  }
+
+  mainMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      headerEl.classList.remove('menu-open');
+    });
+  });
+});
+
+// fix links #
+const relativeLinks = document.querySelectorAll("a[href^='#']");
+
+relativeLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    if (link.getAttribute('href') === '#' || link.getAttribute('href') === '#top') {
+      return;
+    }
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetEl = document.querySelector(targetId);
+    const topOfElement = targetEl.offsetTop - headerEl.offsetHeight - 10;
+
+    window.scroll({ top: topOfElement, behavior: 'smooth' });
+  });
+});
+
+// page menu
+const pageNavBtn = document.querySelector('.page-nav__btn');
+const openPageMenuBtn = document.querySelector('.open-page-menu-btn');
+
+pageNavBtn.addEventListener('click', () => {
+  console.log('helo');
+  sectionHeroEl.classList.remove('page-menu-open');
+});
+
+openPageMenuBtn.addEventListener('click', () => {
+  console.log('hellllllo');
+  sectionHeroEl.classList.add('page-menu-open');
+});
